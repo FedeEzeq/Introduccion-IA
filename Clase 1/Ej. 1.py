40913666df41e8080ec85dc4b57bfb1b76fa6bb5
FedeEzeq <77301813+FedeEzeq@users.noreply.github.com>
2021-05-04 00:42:43 -0300

    Subida Ej. 1
    Ejercicio de la clase 1, usado a modo de prueba para ver funcionamiento de GitHub

---
+++ Clase 1/Ej. 1.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Apr 29 20:35:30 2021
+
+@author: Usuario
+"""
+
+import numpy as np
+import sys
+
+'''
+np.array ([1,2,3])
+
+a = np.ones(3)
+print (a)
+
+b = np.ones((10,12))
+print (b)
+
+c = np.eye(2)
+print(c)
+'''
+
+p = 2 #3,4,5,6
+x = np.array ([[1,2,3],[2,3,4],[5,6,7]])
+print (d)
+
+print (np.sum(np.abs(x)**p, axis = 1)**(1/p))
